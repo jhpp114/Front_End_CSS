6e0c32cdd5f524a32b1395db5acf500ab250af66
jhpp114 <jhpp114@uw.edu>
2020-07-03 13:34:28 +0900

    local storage save remove done

--- todolist/js/app.js
+++ todolist/js/app.js
@@ -5,6 +5,7 @@ const todolist_ul = document.querySelector('.todolist_ul');
 const todo_filter = document.querySelector('#todo_filter');
 
 // Event Listener
+document.addEventListener("DOMContentLoaded", loadDataFromLocalStorage);
 todolist_button.addEventListener("click", addList);
 todolist_ul.addEventListener("click", completeAndRemove);
 todo_filter.addEventListener("click", filterLists);
@@ -55,7 +56,8 @@ function completeHelper(the_event) {
 function removeHelper(the_event) {
     // let deleteTarget = event.target.parentNode;
     the_event.classList.add('delete');
-        // console.log(deleteTarget);
+    // console.log(the_event);
+    localstorage_list_remove(the_event);
     the_event.addEventListener('transitionend', () => {
         the_event.remove();
     });
@@ -80,6 +82,8 @@ function addAsListHelper(todo_value) {
     todoListDiv.classList.add('todo_list');
     // get values
     let todolist_value = todo_value;
+    // save it in local storage
+    setInLocalStorage(todolist_value);
     // create li elements and put value into li
     let li_tags = document.createElement('li');
     li_tags.innerText = todolist_value;
@@ -94,6 +98,7 @@ function addAsListHelper(todo_value) {
     trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
     trashBtn.classList.add('trash_btn');
     todoListDiv.appendChild(trashBtn);
+
     // append it to ul
     todolist_ul.appendChild(todoListDiv);
     // console.log(todoListDiv);
@@ -105,4 +110,65 @@ function clearInputHelper() {
     todolist_add_input.value = "";
 }
 
+function setInLocalStorage(todo_list) {
+    let todo_list_local;
+    console.log(todo_list);
+    if (localStorage.getItem("todo_list_local") === null) {
+        todo_list_local = [];
+    } else {
+        // convert it to json obj
+        todo_list_local = JSON.parse(localStorage.getItem("todo_list_local"));
+    }
+    // push it in json obj
+    todo_list_local.push(todo_list);
+    localStorage.setItem("todo_list_local", JSON.stringify(todo_list_local));
+}
+
+function loadDataFromLocalStorage() {
+    let todo_list_local;
+    if (localStorage.getItem("todo_list_local") === null) {
+        todo_list_local = [];
+    } else {
+        todo_list_local = JSON.parse(localStorage.getItem("todo_list_local"));
+    }
+    if (todo_list_local.length === 0) {
+        return;
+    } else {
+        todo_list_local.forEach((each_todo) => {
+            let todoListDiv = document.createElement('div');
+            todoListDiv.classList.add('todo_list');
+            // get values
+            let todolist_value = each_todo;
+            // create li elements and put value into li
+            let li_tags = document.createElement('li');
+            li_tags.innerText = todolist_value;
+            todoListDiv.appendChild(li_tags);
+            // create complete button
+            let completeBtn = document.createElement('button');
+            completeBtn.innerHTML = '<i class="far fa-thumbs-up"></i>';
+            completeBtn.classList.add('completed_btn');
+            todoListDiv.appendChild(completeBtn);
+            // create trash button
+            let trashBtn = document.createElement('button');
+            trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
+            trashBtn.classList.add('trash_btn');
+            todoListDiv.appendChild(trashBtn);
+            // append it to ul
+            todolist_ul.appendChild(todoListDiv);
+        });
+    }
+}
+
+function localstorage_list_remove(removeItem) {
+    let todo_list_local;
+    if (localStorage.getItem("todo_list_local") === null) {
+        todo_list_local = [];
+    } else {
+        todo_list_local = JSON.parse(localStorage.getItem("todo_list_local"));
+    }
+    let targetItem = removeItem.childNodes[0].innerText;
+    let targetIndex = todo_list_local.indexOf(targetItem);
+    todo_list_local.splice(targetIndex, 1);
+    localStorage.setItem("todo_list_local", JSON.stringify(todo_list_local));
+}
 
