aeb07027fad596f19d12a73ada57a86fb0b4d0d6
jhpp114 <jhpp114@uw.edu>
2020-07-23 09:27:42 +0900

    view 950px done

--- Netflix/js/app.js
+++ Netflix/js/app.js
@@ -3,8 +3,6 @@ const parent_toggle_data = document.querySelector('.faq_list ul');
 parent_toggle_data.addEventListener('click', toggle_data);
 
 function toggle_data(e) {
-    //console.log(e.target);
-    //console.log(e.target.nextSibling.nextSibling);
     let dataToToggle = e.target.nextSibling.nextSibling;
     dataToToggle.classList.toggle("toggle_data");
 }
