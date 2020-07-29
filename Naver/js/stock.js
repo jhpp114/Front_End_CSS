85c2b749e6dda9a53d10419ad009fdef176c967d
jhpp114 <jhpp114@uw.edu>
2020-07-29 17:16:56 +0900

    stock functionality is done need style

--- Naver/js/stock.js
+++ Naver/js/stock.js
@@ -1,9 +1,9 @@
 const STOCK_API_KEY = `bsgf0afrh5r8gpgm5png`;
 // DOM Elements
 const COMPANY_IMG = document.querySelector('.stock_name_img');
-const COMPANY_LAST_UPDATE = document.querySelector('.stock_last_update');
+const COMPANY_TICKER = document.querySelector('.stock_ticker');
 const COMPANY_STOCK_OPEN = document.querySelector('.stock_open');
-const COMPANY_STOCK_CLOSE = document.querySelector('.stock_close');
+const COMPANY_STOCK_CURRENT = document.querySelector('.stock_current');
 const COMPANY_STOCK_HIGH = document.querySelector('.stock_high');
 const COMPANY_STOCK_LOW = document.querySelector('.stock_low');
 
@@ -23,23 +23,79 @@ function getCompanyName() {
 };
 
 async function generate_stock_data() {
-    await Promise.all([
-        // api related to low(l) high(h) open(o) current(c)
-        await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=AAPL&token=${STOCK_API_KEY}`)
-        // company information
-    ,   await fetch(`https://finnhub.io/api/v1/quote?symbol=AAPL&token=${STOCK_API_KEY}`)
-    ])
-    .then(async function (responses) {
-        let resultJson = [];
-        for (let i = 0; i < responses.length; i++) {
-            resultJson[i] = await responses[i].json();
+    let company = getCompanyName();
+    let index = 0;
+    setInterval(async ()=>{
+        try {
+            const responses =  Promise.all([
+                fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${company[index]}&token=${STOCK_API_KEY}`)
+                .then(response => response.json())
+            ,   fetch(`https://finnhub.io/api/v1/quote?symbol=${company[index]}&token=${STOCK_API_KEY}`)    
+                .then(response => response.json())
+            ]);
+            // console.log(responses);
+            let data = await responses;
+            console.log(data);
+            let company_name = data[0].name;
+            let company_logo = data[0].logo;
+            let company_ticker = data[0].ticker;
+            let stock_open_price = data[1].o;
+            let stock_current_price = data[1].c;
+            let stock_high_price = data[1].h.toString().substring(0,6);
+            let stock_low_price = data[1].l.toString().substring(0,6);
+            COMPANY_IMG.innerHTML = `<img src="${company_logo}" width="60px"> <p>Company: ${company_name} </p>`;
+            COMPANY_TICKER.textContent = `Ticker: ${company_ticker}`;
+            COMPANY_STOCK_OPEN.textContent = `Open Price at $${stock_open_price}`;
+            COMPANY_STOCK_CURRENT.textContent = `Current Price at $${stock_current_price}`;
+            COMPANY_STOCK_HIGH.textContent = `Today's Highest Price at $${stock_high_price}`;
+            COMPANY_STOCK_LOW.textContent = `Today's Lowest Price at $${stock_low_price}`;
+            if (index < company.length - 1) {
+                index++;
+            } else {
+                index = 0;
+            }
+            // return responses;
+            // return resultJson;
+        } catch (error) {
+            console.log(error);
         }
-        return resultJson;
-    }).then(data => {
-        console.log(data);
-        let company_name = data[0].name;
-        let company_logo = data[0].logo;
-        COMPANY_IMG.innerHTML = `<img src="${company_logo}" width="60px"> <p>Company: ${company_name}</p>`;
-        
-    }).catch(error => console.log(error));
+    }, 4000);
+    
 }
+
+// async function insert_stock_data() {
+//     let data = await generate_stock_data();
+//     let company_name = data[0].name;
+//     let company_logo = data[0].logo;
+//     let company_ticker = data[0].ticker;
+//     let stock_open_price = data[1].o;
+//     let stock_current_price = data[1].c;
+//     let stock_high_price = data[1].h.toString().substring(0,6);
+//     let stock_low_price = data[1].l.toString().substring(0,6);
+//     COMPANY_IMG.innerHTML = `<img src="${company_logo}" width="60px"> <p>Company: ${company_name} </p>`;
+//     COMPANY_TICKER.textContent = `Ticker: ${company_ticker}`;
+//     COMPANY_STOCK_OPEN.textContent = `Open Price at $${stock_open_price}`;
+//     COMPANY_STOCK_CURRENT.textContent = `Current Price at $${stock_current_price}`;
+//     COMPANY_STOCK_HIGH.textContent = `Today's Highest Price at $${stock_high_price}`;
+//     COMPANY_STOCK_LOW.textContent = `Today's Lowest Price at $${stock_low_price}`;
+// }
+
+        
+    
+//     Promise.all([
+//         // api related to low(l) high(h) open(o) current(c)
+        
+//         // company information
+    
+//     ])
+//     .then(async function (responses) {
+//         let resultJson = [];
+//         for (let i = 0; i < responses.length; i++) {
+//             resultJson[i] = responses[i].json();
+//         }
+//         return resultJson;
+//     }).then(data => {
+//         console.log(data);
+        
+//     }).catch(error => console.log(error));
+
