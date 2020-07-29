5b48d02b3eed1a498dd23411528eadb6dc4851b7
jhpp114 <jhpp114@uw.edu>
2020-07-29 01:46:59 -0700

    added style

--- Naver/js/stock.js
+++ Naver/js/stock.js
@@ -18,6 +18,7 @@ function getCompanyName() {
     ,   'BAC'
     ,   'BABA'
     ,   'AAPL'
+    ,   'AMZN'
     ];
     return companies;
 };
@@ -33,7 +34,6 @@ async function generate_stock_data() {
             ,   fetch(`https://finnhub.io/api/v1/quote?symbol=${company[index]}&token=${STOCK_API_KEY}`)    
                 .then(response => response.json())
             ]);
-            // console.log(responses);
             let data = await responses;
             console.log(data);
             let company_name = data[0].name;
@@ -60,42 +60,4 @@ async function generate_stock_data() {
             console.log(error);
         }
     }, 4000);
-    
 }
-
-// async function insert_stock_data() {
-//     let data = await generate_stock_data();
-//     let company_name = data[0].name;
-//     let company_logo = data[0].logo;
-//     let company_ticker = data[0].ticker;
-//     let stock_open_price = data[1].o;
-//     let stock_current_price = data[1].c;
-//     let stock_high_price = data[1].h.toString().substring(0,6);
-//     let stock_low_price = data[1].l.toString().substring(0,6);
-//     COMPANY_IMG.innerHTML = `<img src="${company_logo}" width="60px"> <p>Company: ${company_name} </p>`;
-//     COMPANY_TICKER.textContent = `Ticker: ${company_ticker}`;
-//     COMPANY_STOCK_OPEN.textContent = `Open Price at $${stock_open_price}`;
-//     COMPANY_STOCK_CURRENT.textContent = `Current Price at $${stock_current_price}`;
-//     COMPANY_STOCK_HIGH.textContent = `Today's Highest Price at $${stock_high_price}`;
-//     COMPANY_STOCK_LOW.textContent = `Today's Lowest Price at $${stock_low_price}`;
-// }
-
-        
-    
-//     Promise.all([
-//         // api related to low(l) high(h) open(o) current(c)
-        
-//         // company information
-    
-//     ])
-//     .then(async function (responses) {
-//         let resultJson = [];
-//         for (let i = 0; i < responses.length; i++) {
-//             resultJson[i] = responses[i].json();
-//         }
-//         return resultJson;
-//     }).then(data => {
-//         console.log(data);
-        
-//     }).catch(error => console.log(error));
-
