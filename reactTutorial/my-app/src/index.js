413eb9f60e50b451914bc3ea40b2526101d0cc6b
jhpp114 <jhpp114@uw.edu>
2020-09-21 16:01:37 -0700

    studying react seperate file component

---
+++ reactTutorial/my-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PersonInfo from "./PersonInfo.js"
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(
+  <PersonInfo/>
+, root
+);
