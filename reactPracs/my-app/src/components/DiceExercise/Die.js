7528db9ca114e186255d78a63f0473ae8dd073d9
jhpp114 <jhpp114@uw.edu>
2020-09-30 23:10:23 -0700

    react rolling dice exercise

--- reactPracs/my-app/src/components/DiceExercise/Die.js
+++ reactPracs/my-app/src/components/DiceExercise/Die.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import './Die.css'
 function Die(props) {
+    // if (props.isRolling) {
+    //     const divContainer = document.querySelectorAll('.die-container i');
+    //     divContainer[0].classList.add('isRolling');
+    //     divContainer[1].classList.add('isRolling');
+    // }
     return (
         <div>
-            <i className="fas fa-dice-one"></i>
+            <i className={`dieStyle ${props.dice1}`}></i>
+            <i className={`dieStyle ${props.dice2}`}></i>
         </div>
     )
 }
--- reactPracs/my-app/src/components/DiceExercise/RollDice.js
+++ reactPracs/my-app/src/components/DiceExercise/RollDice.js
@@ -1,25 +1,50 @@
 import React, {Component} from 'react'
 import Die from './Die.js'
+import './RollDice.css'
 class RollDice extends Component {
     constructor() {
         super();
         this.state = {
-            diceState: ""
+            dice1State: "fas fa-dice-one"
+        ,   dice2State: "fas fa-dice-one"
         ,   isRolling: false
         }
     }
     
-    handleClick() {
-        const randomDice = Math.floor(Math.random() * 6) + 1;
+    handleClick = () => {
+        console.log("clicked");
+        const randomDice1 = Math.floor(Math.random() * 6);
+        const randomDice2 = Math.floor(Math.random() * 6);
+        const numberInLetters = ["one", "two", "three", "four", "five", "six"];
+        
+        this.setState({
+            dice1State: `fas fa-dice-${numberInLetters[randomDice1]}`
+        ,   dice2State: `fas fa-dice-${numberInLetters[randomDice2]}`
+        ,   isRolling: true
+        })
+        setTimeout(() => {
+            this.setState({
+                isRolling: false
+            })
+        }, 2000);
     }
 
     render() {
-
+        console.log(this.state.isRolling);
+        let isRolling = this.state.isRolling? "Rolling...." : "Roll"
         return(
-            <div>
+            <div className="rolldice-container">
                 <Die
-                    dice={this.state.diceState}/>
-                <button id="rollBtn">Roll</button>
+                    dice1={this.state.dice1State}
+                    dice2={this.state.dice2State}
+                    isRolling={this.state.isRolling}/>
+                <button 
+                    // className={this.state.isRolling? "disableBtn" : "rollBtn"} 
+                    className="rollBtn"
+                    disabled={this.state.isRolling}
+                    onClick={this.handleClick}>
+                        {isRolling}
+                </button>
             </div>
         )
     }
