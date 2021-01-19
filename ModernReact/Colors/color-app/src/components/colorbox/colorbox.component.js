8bc2e847d6a8076edd35242cd8c9c9b42c6e78ed
jhpp114 <jhpp114@uw.edu>
2021-01-19 15:13:48 -0800

    basic color box style arrangement

--- ModernReact/Colors/color-app/src/components/colorbox/colorbox.component.js
+++ ModernReact/Colors/color-app/src/components/colorbox/colorbox.component.js
@@ -3,10 +3,16 @@ import './colorbox.style.css';
 
 class colorbox extends Component {
     render() {
+        const { background, name } = this.props;
         return (
-            <div style={{backgroundColor: this.props.background}} className='colorbox'>
-                <span>{this.props.name}</span>
-                <span>More</span>
+            <div style={{backgroundColor: background}} className='colorbox'>
+                <div className='copy-container'>
+                    <div className='box-content'>
+                        <span>{name}</span>
+                    </div>
+                    <button className='copy-button'>Copy</button>
+                </div>
+                <span className='see-more'>More</span>
             </div>
         );
     }
