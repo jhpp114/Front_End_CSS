50f8bfac2a6f1d5fb94312e6f2fb1e901075331c
jhpp114 <jhpp114@uw.edu>
2021-01-19 21:05:12 -0800

    added url using react router dom

--- ModernReact/Colors/color-app/src/App.js
+++ ModernReact/Colors/color-app/src/App.js
@@ -1,14 +1,29 @@
+import React, { Component } from 'react'
 import Colors from './seedColors';
 import Palette from './components/palette/palette.component';
 import {generatePalette} from './colorHelper';
 
-function App() {
-  console.log(generatePalette(Colors[1]));
-  return (
-    <div className="App">
-      <Palette palette={generatePalette(Colors[1])}/>
-    </div>
-  );
+import {Route, Switch} from 'react-router-dom';
+//material-ui-colors
+class App extends Component {
+  findPalette(id) {
+    const targetPalette = Colors.find( eachPalette => {
+      return eachPalette.id === id;
+    })
+    return targetPalette;
+  }
+  render() {
+    console.log(generatePalette(Colors[1]));
+    return (
+      <Switch>
+        <Route exact path='/' render={()=><h1>Hello</h1>}/>
+        <Route exact path='/palette/:id' render={(routeParams)=> <Palette palette={generatePalette(this.findPalette(routeParams.match.params.id))}/>}/>
+      </Switch>
+      // <div className="App">
+      //   <Palette palette={generatePalette(Colors[1])}/>
+      // </div>
+    );
+  }
 }
 
 export default App;
