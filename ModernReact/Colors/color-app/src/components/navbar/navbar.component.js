fbdbec207581c37f60245171a3f705bb4057dfef
jhpp114 <jhpp114@uw.edu>
2021-01-19 20:16:01 -0800

    added material ui snack bar with svg icon

--- ModernReact/Colors/color-app/src/components/navbar/navbar.component.js
+++ ModernReact/Colors/color-app/src/components/navbar/navbar.component.js
@@ -4,27 +4,39 @@ import 'rc-slider/assets/index.css';
 import './navbar.style.css';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 class navbar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            format: "hex"
+            format: "hex",
+            open: false
         }
         this.handleChange = this.handleChange.bind(this);
+        this.closeSnackbar = this.closeSnackbar.bind(this);
     }
 
     handleChange(event) {
         this.setState({
-            format: event.target.value
+            format: event.target.value,
+            open: true
         });
         this.props.formatChange(event.target.value);
+    }
+
+    closeSnackbar() {
+        this.setState({
+            open: false
+        });
     }
 
     render() {
         const { level, changeLevel } = this.props;
         return (
-            <div className='navbar'>
+            <nav className='navbar'>
                 <div className='logo'>
                     <a href="#">ReactColorApp</a>
                 </div>
@@ -57,7 +69,32 @@ class navbar extends Component {
                         </MenuItem>
                     </Select>
                 </div>
-            </div>
+                <Snackbar
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                      }}
+                    autoHideDuration={3000}
+                    message={<span id='message-id'>Format Changed!</span>}
+                    open={this.state.open}
+                    ContentProps = {
+                        {"aria-describedby": "message-id"}
+                    }
+                    onClose={this.closeSnackbar}
+                    action={
+                        [
+                            <IconButton
+                                onClick={this.closeSnackbar}
+                                key='close'
+                                aria-label='close'
+                                color='inherit'
+                            >
+                                <CloseIcon/>
+                            </IconButton>
+                        ]
+                    }
+                />
+            </nav>
         );
     }
 }
