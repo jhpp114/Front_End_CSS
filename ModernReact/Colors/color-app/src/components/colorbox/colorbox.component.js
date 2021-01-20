144cee995cd40f8d15c173a25f1679f3ffd87a7d
jhpp114 <jhpp114@uw.edu>
2021-01-19 16:35:03 -0800

    added copy functionality with style

--- ModernReact/Colors/color-app/src/components/colorbox/colorbox.component.js
+++ ModernReact/Colors/color-app/src/components/colorbox/colorbox.component.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
 import './colorbox.style.css';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 class colorbox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isCopied: false
+        }
+        this.handleCopy = this.handleCopy.bind(this);
+    }
+
+    handleCopy() {
+        this.setState({
+            isCopied: true
+        }, () => {
+            setTimeout( ()=>{
+                this.setState({
+                    isCopied: false
+                })
+            }, 1500);
+        });
+    }
+
     render() {
         const { background, name } = this.props;
+        const { isCopied } = this.state;
         return (
-            <div style={{backgroundColor: background}} className='colorbox'>
-                <div className='copy-container'>
-                    <div className='box-content'>
-                        <span>{name}</span>
+            <CopyToClipboard text={background} onCopy={this.handleCopy}>
+                <div style={{backgroundColor: background}} className='colorbox'>
+                    <div style={{backgroundColor:background}} 
+                        className={`copy-overlay ${isCopied? `show`:``}`}
+                    />
+                    <div className={`copy-msg ${isCopied? `show`:``}`}>
+                        <h2 className='copy-msg-header'>Copied!</h2>
+                        <p className='copy-msg-para'>{background}</p>
                     </div>
-                    <button className='copy-button'>Copy</button>
+                    <div className='copy-container'>
+                        <div className='box-content'>
+                            <span>{name}</span>
+                        </div>
+                        <button className='copy-button'>Copy</button>
+                    </div>
+                    <span className='see-more'>More</span>
                 </div>
-                <span className='see-more'>More</span>
-            </div>
+            </CopyToClipboard>
         );
     }
 }
